278020ef8275f5b7e681bc48332277e3e876e8c9
39LPeter <wlone3949@gmail.com>
2023-09-08 10:38:08 -0700

    0x15-file_io

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,43 +1,38 @@
-#include "main.h" 
- #include <unistd.h> 
- #include <sys/types.h> 
- #include <sys/stat.h> 
- #include <fcntl.h> 
- #include <stdlib.h> 
-  
- /** 
-  * read_textfile - reads a text file and prints it to the POSIX standard output 
-  * @filename: name of the file to read 
-  * @letters: number of letters it should read and print 
-  * 
-  * Return: actual number of letters it could read and print 
-  */ 
- ssize_t read_textfile(const char *filename, size_t letters) 
- { 
-         int fd; 
-         ssize_t lenr, lenw; 
-         char *buffer; 
-  
-         if (filename == NULL) 
-                 return (0); 
-         fd = open(filename, O_RDONLY); 
-         if (fd == -1) 
-                 return (0); 
-         buffer = malloc(sizeof(char) * letters); 
-         if (buffer == NULL) 
-         { 
-                 close(fd); 
-                 return (0); 
-         } 
-         lenr = read(fd, buffer, letters); 
-         close(fd); 
-         if (lenr == -1) 
-         { 
-                 free(buffer); 
-                 return (0); 
-         } 
-         lenw = write(STDOUT_FILENO, buffer, lenr); 
-         free(buffer); 
-         if (lenr != lenw) 
-                 return (0); 
-         return (lenw);
+#include "main.h"
+/**
+ * read_textfile - read a textfile.
+ * @filename: name of the file to open.
+ * @letters: number of letters to print in the standar ouput.
+ *
+ * Return: number of letter printed.
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd_read, count_chars, fd_open;
+	char *buf_letters;
+
+	if (filename == NULL)
+		return (0);
+	fd_open = open(filename, O_RDONLY);
+	if (fd_open == -1)
+		return (0);
+	buf_letters = malloc(sizeof(char) * letters);
+	if (buf_letters == NULL)
+		return (0);
+	fd_read = read(fd_open, buf_letters, letters);
+	if (fd_read == -1)
+	{
+		free(buf_letters);
+		return (0);
+	}
+	count_chars = write(STDOUT_FILENO, buf_letters, fd_read);
+	if (count_chars == -1)
+	{
+		free(buf_letters);
+		return (0);
+	}
+	close(fd_open);
+	free(buf_letters);
+	return (count_chars);
+}
--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,39 +1,41 @@
-#include "main.h" 
-  
- /** 
-  * append_text_to_file - appends text at the end of a file 
-  * @filename: filename. 
-  * @text_content: added content. 
-  * 
-  * Return: 1 if the file exists. -1 if the fails does not exist 
-  * or if it fails. 
-  */ 
- int append_text_to_file(const char *filename, char *text_content) 
- { 
-         int fd; 
-         int nletters; 
-         int rwr; 
-  
-         if (!filename) 
-                 return (-1); 
-  
-         fd = open(filename, O_WRONLY | O_APPEND); 
-  
-         if (fd == -1) 
-                 return (-1); 
-  
-         if (text_content) 
-         { 
-                 for (nletters = 0; text_content[nletters]; nletters++) 
-                         ; 
-  
-                 rwr = write(fd, text_content, nletters); 
-  
-                 if (rwr == -1) 
-                         return (-1); 
-         } 
-  
-         close(fd); 
-  
-         return (1); 
- }
+#include "main.h"
+/**
+ * append_text_to_file - Append text at the end of a file
+ * @filename: name of the file to create.
+ * @text_content: text to write inside the file.
+ *
+ * Return: 1 if text_content was appened, -1 otherwise.
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fd_open, fd_write;
+
+	if (filename == NULL)
+		return (-1);
+	fd_open = open(filename, O_RDWR | O_APPEND);
+	if (fd_open == -1)
+		return (-1);
+	if (text_content != NULL)
+	{
+		fd_write = write(fd_open, text_content, _strlen(text_content));
+		if (fd_write == -1)
+			return (-1);
+	}
+	close(fd_open);
+	return (1);
+}
+
+/**
+ * _strlen - calculate the lenght of a string.
+ * @str: array of characters.
+ *
+ * Return: lenght of the string.
+ */
+int _strlen(char *str)
+{
+	int count = 0;
+
+	while (*str++)
+		count++;
+	return (count);
+}
