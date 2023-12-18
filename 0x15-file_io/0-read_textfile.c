d19c6e73e7eaa48eb70582ec5b05001987c51f4c
root <wlone3949@gmail.com>
2023-12-18 07:25:49 -0800

    corrections

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -3,38 +3,40 @@
 #include "main.h"
 
 /**
- * Write a function that reads a text file and prints it to the POSIX standard output.
-
-Prototype: ssize_t read_textfile(const char *filename, size_t letters);
-where letters is the number of letters it should read and print
-returns the actual number of letters it could read and print
-if the file can not be opened or read, return 0
-if filename is NULL return 0
-if write fails or does not write the expected amount of bytes, return 0
-*/
+ * read_textfile - that reads a text file and prints
+ * @filename: variable pointer
+ * @letters: size letters
+ * Description: Write a function that reads a text file and prints it
+ * to the POSIX standard output.
+ * Return: the actual number of letters it could read and print, 0 otherwise
+ */
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	ssize_t file, let, l;
 	char *text;
 
-	text= malloc(letters);
-	if (text==NULL)
+	text = malloc(letters);
+	if (text == NULL)
 		return (0);
-	if (filename==NULL)
-		return (0);
-	file= open(filename, O_RDONLY);
 
-	if(file == -1)
+	if (filename == NULL)
+		return (0);
+
+	file = open(filename, O_RDONLY);
+
+	if (file == -1)
 	{
 		free(text);
-		return(0);
+		return (0);
 	}
-        let =read(file,text,letters);
-        l = write(STDOUT_FILENO, text,let)
+
+	let = read(file, text, letters);
+
+	l = write(STDOUT_FILENO, text, let);
 
 	close(file);
-        
-        return(l);
+
+	return (l);
 }	
 
